9e9a3e2ff33ff53fa4855e4b0d2913c0f4860bea
Lais <laissouroch@gmail.com>
2023-09-14 10:47:21 -0300

    implementando mais funcionalidades

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse # reponder uma requisição
 
+
+
 def index(request): #Função para retornar uma requisição
-    return render(request, 'galeria/index.html')
+    dados = {
+    1:{"nome": "Nebulosa de Carina",
+       "legenda": "webbtelescope.org / NASA / James Webb"},
+    2:{"nome": "Galáxia NGC 1079",
+        "legenda": "nasa.org / NASA / Hubble"}
+}
+    return render(request, 'galeria/index.html', {"cards": dados}) #Passando os dados do dicionario para o index.html
 def imagem(request):
     return render(request, 'galeria/imagem.html')
